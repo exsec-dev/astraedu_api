4d599eb2c7e25e5b5febcf1a634c0a5c3c89ff82
Prozhirko Ekaterina <prozhirkoey@it.mos.ru>
2024-03-19 23:34:51 +0300

    Changed port

--- app.js
+++ app.js
@@ -4,37 +4,6 @@ const mysql = require("mysql");
 const app = express();
 app.use(express.json());
 
-const port = 80;
-//let conn;
-
-// function reconnect() {
-//     conn = mysql.createConnection({
-//         host: 'exsec.beget.tech',
-//         user: 'exsec_astraedu',
-//         password: '6BjrIT1*',
-//         database: 'exsec_astraedu'
-//     });
-
-//     conn.connect((err) => {
-//         if (err) {
-//             console.error('Error trying connect to database: ' + err.stack);
-//             setTimeout(reconnect, 2000);
-//         } else {
-//             console.log('CONNECTED DATABASE---------');
-//         }
-//     });
-
-//     conn.on('error', (err) => {
-//         if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-//             reconnect();
-//         } else {
-//             throw err;
-//         }
-//     });
-// }
-
-// reconnect();
-
 const pool = mysql.createPool({
     host: 'exsec.beget.tech',
     user: 'exsec_astraedu',
@@ -45,6 +14,6 @@ const pool = mysql.createPool({
 const handlers = require('./handlers.js')(pool);
 app.use(handlers);
 
-app.listen(port, () => {
-    console.log("ASTRA_EDU_BACK IS RUNNING ON PORT 80---------");
+app.listen( () => {
+    console.log("ASTRA_EDU_BACK IS RUNNING---------");
 })
