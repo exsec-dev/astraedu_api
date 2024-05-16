425386585f3e43bfdf243474e0c9aa10b56431b9
Prozhirko Ekaterina <prozhirkoey@it.mos.ru>
2024-05-17 01:04:58 +0300

    Fix bug

--- handlers.js
+++ handlers.js
@@ -452,6 +452,8 @@ module.exports = (pool) => {
             ${JSON.parse(is_correct) ? `, ${moduleMap[module]} = JSON_SET(${moduleMap[module]}, '$[${chapter}].progress', JSON_EXTRACT(${moduleMap[module]}, '$[${chapter}].progress') + 1)` : ''}
             WHERE username = ?;
         `;
+        console.log(typeof is_correct)
+        console.log(typeof JSON.parse(is_correct))
         pool.query(query, [user], (error, results) => {
             if (error) {
                 console.error('GET error: ' + error.stack);
