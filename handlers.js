281201e214ade3c806f35d64a90827300b9f47eb
Prozhirko Ekaterina <prozhirkoey@it.mos.ru>
2024-05-17 00:09:20 +0300

    Fix bug

--- handlers.js
+++ handlers.js
@@ -448,10 +448,11 @@ module.exports = (pool) => {
         };
         const query = `
             UPDATE modules
-            SET ${moduleMap[module]} = JSON_SET(${moduleMap[module]}, '$.${chapter}.details.${question}', '${answer}')
-            ${is_correct ? `, ${moduleMap[module]} = JSON_SET(${moduleMap[module]}, '$.${chapter}.progress', JSON_EXTRACT(${moduleMap[module]}, '$.${chapter}.progress') + 1)` : ''}
+            SET ${moduleMap[module]} = JSON_SET(${moduleMap[module]}, '$[${chapter}].details.[${question}]', '${answer}')
+            ${is_correct ? `, ${moduleMap[module]} = JSON_SET(${moduleMap[module]}, '$[${chapter}].progress', JSON_EXTRACT(${moduleMap[module]}, '$[${chapter}].progress') + 1)` : ''}
             WHERE username = ?;
         `;
+        console.log(query)
         pool.query(query, [user], (error, results) => {
             if (error) {
                 console.error('GET error: ' + error.stack);
